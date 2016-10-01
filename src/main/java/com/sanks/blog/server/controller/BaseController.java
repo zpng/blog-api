a266694f88b102c7563f70555f8998471b4180e3
zhangpeng <zhangpeng@fenbi.com>
2016-10-01 14:22:36 +0800

    FIX: fix get remote address from proxy bug

--- src/main/java/com/sanks/blog/server/controller/BaseController.java
+++ src/main/java/com/sanks/blog/server/controller/BaseController.java
@@ -14,10 +14,27 @@ import javax.servlet.http.HttpServletRequest;
  * @author ldap
  */
 public class BaseController {
+
     @Autowired
     private HttpServletRequest request;
 
-    protected String getIp(){
-        return request.getRemoteAddr();
+    protected String getIp() {
+        String ip = request.getHeader("X-Forwarded-For");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
     }
 }
